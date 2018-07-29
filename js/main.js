a68750f116606e19c1eadd432b9b977f4b622344
leon20121005 <leon20121005@gmail.com>
2018-07-30 01:22:54 +0800

    fix the bug in index and archive, refactor js files

---
+++ js/main.js
@@ -0,0 +1,44 @@
+function setBackToTopButtonRight()
+{
+    containerRight = ($(window).width() - $(".container").outerWidth()) / 2;
+    $("#back-to-top-button").css("right", containerRight - 120);
+}
+
+function animateNavbar(threshold)
+{
+    if ($(window).scrollTop() > threshold)
+    {
+        $("#nav").addClass("animated");
+    }
+    else
+    {
+        $("#nav").removeClass("animated");
+    }
+}
+
+function showBackToTopButton()
+{
+    if ($(window).scrollTop() > 20)
+    {
+        $("#back-to-top-button").fadeIn(800);
+    }
+    else
+    {
+        $("#back-to-top-button").fadeOut(800);
+    }
+}
+
+function scrollWindow(target)
+{
+    if (target != "#")
+    {
+        if ($(target).offset().top - 56 > 0)
+        {
+            $("html, body").animate({scrollTop: $(target).offset().top - 56}, 2000, "easeInOutExpo");
+        }
+        else
+        {
+            $("html, body").animate({scrollTop: 0}, 2000, "easeInOutExpo");
+        }
+    }
+}
